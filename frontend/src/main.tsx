929379370f2419817d7647bcc947f2f377cb7abb
matchandfight <matchandfight@users.noreply.github.com>
2025-12-09 16:59:04 +0100

    Fix CRÍTICO: Eliminar conflictos CSS - Chronelia ahora funciona correctamente

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -29,11 +29,9 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-/* Theme variables */
-import './theme/variables.css';
-import './theme/design-system.css';
-import './theme/global.css';
+/* Theme variables - Orden corregido para Chronelia */
 import './theme/mobile.css';
+import './theme/global.css'; /* Global debe ir al final para sobreescribir */
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
